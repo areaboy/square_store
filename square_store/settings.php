f35c3cb594e22091a4ddae9dabb3ea6aacd2e808
Esedo Fredrick <50068759+areaboy@users.noreply.github.com>
2023-06-06 11:09:40 +0100

    Update settings.php

--- square_store/settings.php
+++ square_store/settings.php
@@ -1,11 +1,11 @@
 ﻿<?php
 
 //error_reporting(0);
-// Set Square Payments Access Token
+// Set Square Sandbox Access Token
 
-$square_accesstoken=  "EAAAEBL62qwrAPJEYtx2Xs0_i18cUJ5giinQUOjPt1vIoeH89TRLlfa1CxD78kIy";
+$square_accesstoken=  "";
 
 //  Set Business Locations Id
-$location_id='L3T69B6YNR9V2';
+$location_id='';
 
 ?>
